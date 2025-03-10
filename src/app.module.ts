c59b8bc417214218a83a36ef323a39f360e609df
Hekit <kubondavid@seznam.cz>
2025-03-10 16:17:22 -0600

    add health endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,17 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { UserModule } from './user/user.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/apoco-code-challenge'),
+    MongooseModule.forRoot(
+      process.env.MONGO_URI || 'mongodb://localhost:27017/apoco-code-challenge',
+    ),
     PokemonModule,
     UserModule,
   ],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
